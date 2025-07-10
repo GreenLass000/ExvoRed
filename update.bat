f0bd6308a849124511149aafd9bfdd9af0857199
GreenLass000 <marcosnavarromartin@gmail.com>
2025-07-10 21:50:59 +0200

    Add update start and backup (empty)

--- update.bat
+++ update.bat
@@ -2,3 +2,10 @@
 
 REM This script is used to update the ExvoRed project files.
 git pull origin main
+
+echo Cleaning up node_modules and package-lock.json...
+if exist node_modules rmdir /s /q node_modules
+if exist package-lock.json del package-lock.json
+
+echo Installing dependencies...
+npm install
