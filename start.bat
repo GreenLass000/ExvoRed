db8bf52f8c93cd9ea08b4774f18569a905bcdc5f
GreenLass000 <marcosnavarromartin@gmail.com>
2025-07-10 21:49:33 +0200

    Add update start and backup (empty)

---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start cmd /k "npm run api:dev"
+start cmd /k "npm run dev"
---
+++ update.bat
@@ -0,0 +1,4 @@
+@echo off
+
+REM This script is used to update the ExvoRed project files.
+git pull origin main
