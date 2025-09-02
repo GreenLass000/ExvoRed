4c14f5ac45fe2bd07ac41ab8d220bc5202e09608
GreenLass000 <marcosnavarromartin@gmail.com>
2025-09-02 16:09:52 +0200

    Deleted database migrations and backups from git

--- api/db/migrations/0003_lonely_professor_monster.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `catalog` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `character` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `divinity` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `exvoto` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `miracle` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `sem` ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP;
